db6a81624626e4e2665cc50f9bb6bd6338f62afb
Programmer <Programmer@ua>
2025-10-01 15:14:48 +0300

    отримання даних з newsapi.org та вивід на головну сторінку

--- newsAPI/blog/views.py
+++ newsAPI/blog/views.py
@@ -1,25 +1,48 @@
 import sys
 
+import requests
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseNotFound, Http404, HttpResponse, HttpResponseBadRequest, HttpResponseServerError, \
     HttpResponseForbidden
 from django.shortcuts import render, redirect
 from django.template.defaulttags import lorem
+from django.views.generic import ListView
 
 from blog.models import Post
+from newsAPI import settings
 
 
 def index(request):
-    from faker import Faker
+    # from faker import Faker
 
-    fake = Faker(locale='uk_UA')
-    posts = ['post1', 'post2', 'post3', 'post4', 'post5', 'post6']
-    list_posts = [{'title': post.capitalize(),
-                   'data': fake.date_time(),
-                   'body': fake.text(200)} for post in posts]
+    # fake = Faker(locale='uk_UA')
+    # posts = ['post1', 'post2', 'post3', 'post4', 'post5', 'post6']
+
+    # list_posts = [{'title': post.capitalize(),
+    #                'data': fake.date_time(),
+    #                'body': fake.text(200)} for post in posts]
+
+    api_key = settings.NEWS_API_KEY  # оголосіть у .env
+    url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}'
+    resp = requests.get(url).json()
+
+    print(resp)
+
+    list_posts = resp.get('articles', [])
     context = {
         'posts': list_posts
     }
     return render(request, 'blog/index.html', context)
+
+# class NewsListView(LoginRequiredMixin, ListView):
+# 	template_name = 'index.html'
+# 	context_object_name = 'articles'
+#
+# 	def get_queryset(self):
+# 		api_key = settings.NEWS_API_KEY  # оголосіть у .env
+# 		url = f'https://newsapi.org/v2/top-headlines?country=ua&apiKey={api_key}'
+# 		resp = requests.get(url).json()
+# 		return resp.get('articles', [])
 
 
 def archive(request, year):
@@ -27,6 +50,7 @@ def archive(request, year):
         # raise Http404() # відображення сторінки з кодом 404 (функція page_not_found(request, exception))
         # return redirect('posts') # редірект з кодом 302 - url змінився тимчасово
         return redirect('home', permanent=True) # редірект з кодом 301 - постійний url permanent=True
+
     return HttpResponse(f'<h1>Архів за {year} рік</h1>')
 
 
